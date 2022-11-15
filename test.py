9353d1264402382baff06b61c75bb267e2b08887
xandpro <majdb594@gmail.com>
2022-11-15 17:51:14 +0200

    - added dict to tuple function - added tuple to dict fucntion

--- test.py
+++ test.py
@@ -6,23 +6,29 @@ state = {
                     ['P', 'P', 'P', 'P'], ],
             "taxis": {'taxi 1': {"location": (3, 3),
                                  "fuel": 15,
-                                 "capacity": 2},
+                                 "max_fuel": 15,
+                                 "capacity": 2,
+                                 "on_board": []},
                       'taxi 2': {"location": (3, 3),
                                  "fuel": 15,
-                                 "capacity": 2}
+                                 "max_fuel": 15,
+                                 "capacity": 2,
+                                 "on_board": []}
                       },
 
             "passengers": {'Yossi': {"location": (2, 3),
-                                     "destination": (2, 3)},
+                                     "destination": (2, 3),
+                                     "picked up": False},
                            'Moshe': {"location": (1, 0),
-                                     "destination": (0, 0)}
+                                     "destination": (0, 0),
+                                     "picked up": False}
                            }
         }
 
 
 def dict_to_tuples(state):
     res = []
-    if type(state) == list and type(state[0]) == list:
+    if type(state) == list and state and type(state[0]) == list:
         r = []
         for l in state:
             r.append(tuple(l))
@@ -41,18 +47,21 @@ t = dict_to_tuples(state)
 def tuple_to_dict(state):
     a = {}
     p = {}
-    q = {}
+    taxis = {}
 
     a[state[0][0]] = state[0][1]
     a[state[1][0]] = p
     pasgs = state[1][1]
     for t in pasgs:
-        p[t[0]] = {t[1][0][0]: t[1][0][1], t[1][1][0]: t[1][1][1]}
-    a[state[2][0]] = q # {taxis : {}}
-    for taxi in state[2][1]:
-
-
-    return None
-
-print(tuple_to_dict(t))
+        p[t[0]] = {t[1][0][0]: t[1][0][1],
+                   t[1][1][0]: t[1][1][1],
+                   t[1][2][0]: t[1][2][1]}
+    a[state[2][0]] = taxis # {taxis : {}}
+    for t in state[2][1]:
+        taxis[t[0]] = {t[1][0][0]: t[1][0][1],
+                       t[1][1][0]: t[1][1][1],
+                       t[1][2][0]: t[1][2][1],
+                       t[1][3][0]: t[1][3][1],
+                       t[1][4][0]: t[1][4][1]}
+    return a
 
